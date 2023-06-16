4e87737681ce09529248b523dd647f2243d9d901
Rhett <rcowen@stanford.edu>
2023-06-16 11:43:44 -0700

    whoops was doing http not https for link

--- react-frontend/src/utils/constants.tsx
+++ react-frontend/src/utils/constants.tsx
@@ -6,4 +6,4 @@ export const SERVER_URL = "http://localhost:8080";
 export const CLIENT_ID = "Iv1.e11b4b8b607c5121";
 
 // export const DOMAIN = "http://localhost:3000";
-export const DOMAIN = "http://www.gpg.bio";
+export const DOMAIN = "https://www.gpg.bio";
